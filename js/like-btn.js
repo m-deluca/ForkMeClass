b91b66acbb146b66979e3d9cf99878d03772d0d1
Daniela <dan.elasam@gmail.com>
2017-12-03 23:37:12 -0500

    Index-LikeButton
    Add a new Like Button

---
+++ js/like-btn.js
@@ -0,0 +1,11 @@
+//  $('.like-btn').on('click', function() {
+//  	console.log('it sworking');
+//     $(this).toggleClass('is-active');
+// });
+
+function myFunction() {
+ 
+
+element.classList.toggle('is-active');
+
+}
