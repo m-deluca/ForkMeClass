513a76eaac885d014b87e814e6bd1fcad43c1c0b
markel.mairs <markel.mairs@gmail.com>
2019-11-13 10:05:26 -0500

    Updated index.html and added main.js
    Updated index.html to add link to main.js file
    
    Added Code to main.js that will display my name in H1 tag

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+
+//self executing anonymous function
+
+(function () {
+	"use strict";
+
+	console.log("fired");
+
+	document.body.innerHTML = "<h1>Markel Mairs</h1>";
+})();
+
+
